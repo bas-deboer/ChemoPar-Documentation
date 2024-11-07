885b2a5bd1642f7d117ebbca4d2d1e4ff89c1390
bas-deboer <basdeboer12@gmail.com>
2024-11-07 22:11:01 +0100

    logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,6 +30,10 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+html_logo = "_static/ChemoPar_logo.png"
+html_favicon = "_static/favicon.ico"
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
